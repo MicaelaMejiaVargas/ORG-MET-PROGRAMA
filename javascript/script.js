8339af1dec7365e6accf788e60e9f6b42a36da66
MicaelaMejiaVargas <mejiavargasmicaela@gmail.com>
2024-08-11 20:48:49 -0300

    creacion de archivos

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+// const nombreUno = document.getElementById("#nameUser");
+// const botonEnviar = document.getElementById("#btnEnviar");
+// const parrafoUno = document.createElement("p");
+// parrafoUno.className = "text-bg-success p-3 fixed-top";
+
+// const mostrarMensajito = function (botonEnviar){}
+//     this.name = "Something Good";
+//     this.onclick1 = function (event) {
+//       console.log(this.name); // undefined, as this is the element
+//     };
+//     this.onclick2 = function (event) {
+//       console.log(this.name); // 'Something Good', as this is the binded Something object
+//     };
+//     element.addEventListener("click", this.onclick1, false);
+//     element.addEventListener("click", this.onclick2.bind(this), false); // Trick
+  
+
+//     botonEnviar.addEventListener("click", );
+// };
+  
