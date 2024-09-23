9c17fb7d174038ea7603d08d5ef5e641e12c5193
solmsl <mejiasilesol@gmail.com>
2024-09-23 18:43:59 -0300

    Co-authored-by: MicaelaMejiaVargas <MicaelaMejiaVargas@users.noreply.github.com> Co-authored-by: Cande <Cnsgnicole@users.noreply.github.com>

--- javascript/asuntoscorporativos.js
+++ javascript/asuntoscorporativos.js
@@ -1,3 +1,4 @@
+
 function CambioTexto() {
     const valorSeleccionado = document.getElementById("valor").value;
     const preguntas = document.querySelectorAll(".preguntas, .opciones");
@@ -10,3 +11,66 @@ function CambioTexto() {
         });
     }
 }
+
+const respuestasCorrectas = [true, false, true, true, true];
+
+
+function mostrarSiguientePregunta(preguntaActual) {
+    const siguientePregunta = document.getElementById('pregunta' + (preguntaActual + 1));
+    if (siguientePregunta) {
+        siguientePregunta.classList.remove('oculto');
+        siguientePregunta.classList.add('fade', 'show');
+    }
+}
+
+
+function responder(preguntaNumero) {
+    mostrarSiguientePregunta(preguntaNumero);
+}
+
+document.getElementById('evaluarBtn').addEventListener('click', function () {
+    const todasPreguntas = document.querySelectorAll('.container-pregunta');
+    let todasContestadas = true;
+    let respuestasUsuario = [];
+    
+    
+    todasPreguntas.forEach((pregunta, index) => {
+        const radios = pregunta.querySelectorAll('input[type="radio"]');
+        const contestada = Array.from(radios).some(radio => radio.checked);
+
+    
+        if (contestada) {
+            const respuestaSeleccionada = Array.from(radios).find(radio => radio.checked).value;
+
+            
+            const esVerdadero = respuestaSeleccionada === "true";  
+            respuestasUsuario.push(esVerdadero);
+        } else {
+            todasContestadas = false;
+            alert(`Por favor, responde la pregunta ${index + 1}`);
+        }
+    });
+
+    
+    if (todasContestadas) {
+        let respuestasCorrectasTotal = 0;
+
+        
+        respuestasUsuario.forEach((respuesta, index) => {
+            if (respuesta === respuestasCorrectas[index]) {
+                respuestasCorrectasTotal++;
+            }
+        });
+
+        
+        const contadorRespuestas = document.getElementById('contadorRespuestas');
+        contadorRespuestas.textContent = `Respuestas correctas: ${respuestasCorrectasTotal} de ${respuestasCorrectas.length}`;
+
+        alert(`Respondiste correctamente ${respuestasCorrectasTotal} de ${respuestasCorrectas.length} preguntas.`);
+        
+        
+        if (respuestasCorrectasTotal === respuestasCorrectas.length) {
+            window.location.href = "atencionalcliente2.html";
+        }
+    }
+});
